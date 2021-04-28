e0373f466231fc18da0663b48451b38d5dd57000
Eva <eva.alonso.ortiz@gmail.com>
2021-04-27 21:26:33 -0400

    new datasets with T1w + updated scripts

--- batch_process.sh
+++ batch_process.sh
@@ -18,22 +18,130 @@ cd ..
 ################
 
 for ((i=0; i< ${#subj_list[@]}; i++ )); do
+
+    # convert dicoms to nifti
     dcm2niix -o nifti/${subj_list[$i]} -m y ${subj_list[$i]}
-    rtSHIM_file=$(ls nifti/${subj_list[$i]}/*.nii | grep rtSHIM)
 
-    # average across echoes
-    sct_maths -i $rtSHIM_file -mean t -o nifti/${subj_list[$i]}/mgre_averaged.nii.gz
+    # save filename variables
+    T1W_file=$(ls nifti/${subj_list[$i]}/*.nii | grep T1w)
 
-    # get cord centerline
-    sct_get_centerline -i nifti/${subj_list[$i]}/mgre_averaged.nii.gz -c t2 -o nifti/${subj_list[$i]}/mgre_averaged_centerline
+    NOSHIM_file=$(ls nifti/${subj_list[$i]}/*.nii | grep NOSHIM )
 
-    # create a binary cylindrical mask around the centerline
-    sct_create_mask -i nifti/${subj_list[$i]}/mgre_averaged.nii.gz -p centerline,nifti/${subj_list[$i]}/mgre_averaged_centerline.nii.gz -size 17mm -f cylinder -o nifti/${subj_list[$i]}/mgre_averaged_seg.nii.gz
+    staticzSHIM_RUN1_file=$(ls nifti/${subj_list[$i]}/*.nii | grep staticzSHIM | grep run1)
+    staticzSHIM_RUN2_file=$(ls nifti/${subj_list[$i]}/*.nii | grep staticzSHIM | grep run2)
+    staticzSHIM_RUN3_file=$(ls nifti/${subj_list[$i]}/*.nii | grep staticzSHIM | grep run3)
 
-    # unzip the image volumes we wish to keep
-    gunzip nifti/${subj_list[$i]}/mgre_averaged_seg.nii.gz -df
+    rtSHIM_RUN1_file=$(ls nifti/${subj_list[$i]}/*.nii | grep rtSHIM | grep run1)
+    rtSHIM_RUN2_file=$(ls nifti/${subj_list[$i]}/*.nii | grep rtSHIM | grep run2)
+    rtSHIM_RUN3_file=$(ls nifti/${subj_list[$i]}/*.nii | grep rtSHIM | grep run3)
 
-    # delete the other images
-    rm nifti/${subj_list[$i]}/mgre_averaged_centerline.csv
-    rm nifti/${subj_list[$i]}/mgre_averaged_centerline.nii.gz
+    # segment the spinal cord
+    sct_deepseg_sc -i $T1W_file -c t1 -o nifti/${subj_list[$i]}/spinal_seg.nii.gz -qc nifti/${subj_list[$i]}/qc
+
+    # average MGRE across echoes
+    sct_maths -i $NOSHIM_file -mean t -o nifti/${subj_list[$i]}/noSHIM_averaged.nii.gz
+
+    sct_maths -i $staticzSHIM_RUN1_file -mean t -o nifti/${subj_list[$i]}/staticzSHIM_run1_averaged.nii.gz
+    sct_maths -i $staticzSHIM_RUN2_file -mean t -o nifti/${subj_list[$i]}/staticzSHIM_run2_averaged.nii.gz
+    sct_maths -i $staticzSHIM_RUN3_file -mean t -o nifti/${subj_list[$i]}/staticzSHIM_run3_averaged.nii.gz
+
+    sct_maths -i $rtSHIM_RUN1_file -mean t -o nifti/${subj_list[$i]}/rtSHIM_run1_averaged.nii.gz
+    sct_maths -i $rtSHIM_RUN2_file -mean t -o nifti/${subj_list[$i]}/rtSHIM_run2_averaged.nii.gz
+    sct_maths -i $rtSHIM_RUN3_file -mean t -o nifti/${subj_list[$i]}/rtSHIM_run3_averaged.nii.gz
+
+    # register mean GRE to T1w scan
+    sct_register_multimodal -i nifti/${subj_list[$i]}/noSHIM_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_noSHIM_averaged
+
+    sct_register_multimodal -i nifti/${subj_list[$i]}/staticzSHIM_run1_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_staticzSHIM_run1_averaged
+    sct_register_multimodal -i nifti/${subj_list[$i]}/staticzSHIM_run2_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_staticzSHIM_run2_averaged
+    sct_register_multimodal -i nifti/${subj_list[$i]}/staticzSHIM_run3_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_staticzSHIM_run3_averaged
+
+    sct_register_multimodal -i nifti/${subj_list[$i]}/rtSHIM_run1_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_rtSHIM_run1_averaged
+    sct_register_multimodal -i nifti/${subj_list[$i]}/rtSHIM_run2_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_rtSHIM_run2_averaged
+    sct_register_multimodal -i nifti/${subj_list[$i]}/rtSHIM_run3_averaged.nii.gz -d nifti/${subj_list[$i]}/spinal_seg.nii.gz -ofolder nifti/${subj_list[$i]}/warp_rtSHIM_run3_averaged
+
+    # apply inverse transformation to bring seg to the space of the GRE T2*
+    # note: this step isn't necessary, sct_register_multimodal already seems to output registered images
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_noSHIM_averaged/warp_spinal_seg2noSHIM_averaged.nii.gz -x linear -d nifti/${subj_list[$i]}/noSHIM_averaged.nii.gz -o nifti/${subj_list[$i]}/spinal_seg_reg.nii.gz
+
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_staticzSHIM_run1_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2staticzSHIM_run1.nii.gz
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_staticzSHIM_run2_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2staticzSHIM_run2.nii.gz
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_staticzSHIM_run3_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2staticzSHIM_run3.nii.gz
+
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_rtSHIM_run1_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2rtSHIM_run1.nii.gz
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_rtSHIM_run2_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2rtSHIM_run2.nii.gz
+    #sct_apply_transfo -i nifti/${subj_list[$i]}/spinal_seg.nii.gz -w nifti/${subj_list[$i]}/warp_rtSHIM_run3_averaged2T1w -x linear -o nifti/${subj_list[$i]}/spinal_seg2rtSHIM_run3.nii.gz
+
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $NOSHIM_file -t
+
+    # save file names to variable once
+    shopt -s nullglob
+    filenames=(vol*)
+    shopt -u nullglob
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_noSHIM_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/no_shim_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $staticzSHIM_RUN1_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_staticzSHIM_run1_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/staticzSHIM_run1_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $staticzSHIM_RUN2_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_staticzSHIM_run2_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/staticzSHIM_run2_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $staticzSHIM_RUN3_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_staticzSHIM_run3_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/staticzSHIM_run3_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $rtSHIM_RUN1_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_rtSHIM_run1_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/rtSHIM_run1_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $rtSHIM_RUN2_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_rtSHIM_run2_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/rtSHIM_run2_extract_metric.csv -append 1
+    done
+    rm vol*
+
+    # split multiecho file, this is required for sct_extract_metric
+    fslsplit $rtSHIM_RUN3_file -t
+
+    # extract the mean across slices
+    for ((j=0; j< ${#filenames[@]}; j++ )); do
+      sct_extract_metric -i ${filenames[$j]} -f nifti/${subj_list[$i]}/warp_rtSHIM_run3_averaged/spinal_seg_reg.nii.gz -method wa -o nifti/${subj_list[$i]}/rtSHIM_run3_extract_metric.csv -append 1
+    done
+    rm vol*
+
 done
+
+# run matlab script to plot the data
+matlab -nodisplay -nodesktop -r "run('./plot_data.m');exit;"
---
+++ plot_data.m
@@ -0,0 +1,63 @@
+
+dir_list = dir('nifti');
+dir_list=dir_list(~ismember({dir_list.name},{'.','..'}));
+
+
+for k = 1:size(dir_list,1)
+
+    NOshim = readtable(['nifti/' dir_list(k).name '/no_shim_extract_metric.csv']);
+    STATICzshim_run1 = readtable(['nifti/' dir_list(k).name '/staticzSHIM_run1_extract_metric.csv']);
+    STATICzshim_run2 = readtable(['nifti/' dir_list(k).name '/staticzSHIM_run2_extract_metric.csv']);
+    STATICzshim_run3 = readtable(['nifti/' dir_list(k).name '/staticzSHIM_run3_extract_metric.csv']);
+    rtshim_run1 = readtable(['nifti/' dir_list(k).name '/rtSHIM_run1_extract_metric.csv']);
+    rtshim_run2 = readtable(['nifti/' dir_list(k).name '/rtSHIM_run2_extract_metric.csv']);
+    rtshim_run3 = readtable(['nifti/' dir_list(k).name '/rtSHIM_run3_extract_metric.csv']);
+
+    for i = 1:size(NOshim,1)
+        mean_STATICzshim(i) = mean([STATICzshim_run1.WA__(i), STATICzshim_run2.WA__(i), STATICzshim_run3.WA__(i)]);
+    end
+    mean_STATICzshim = mean_STATICzshim';
+
+    for i = 1:size(NOshim,1)
+        mean_rtshim(i) = mean([rtshim_run1.WA__(i), rtshim_run2.WA__(i), rtshim_run3.WA__(i)]);
+    end
+    mean_rtshim = mean_rtshim';
+
+
+    marker = {'-o';'-x';'-s';'-d';'-*';'-+'};
+    nTE = size(NOshim.WA__,1)/12;
+    
+    figure1 = figure; hold on; j=1;
+    for i = 1:12:size(NOshim,1)
+        subplot(nTE,1,j); 
+        hold on;
+        plot(NOshim.WA__(i:i+11),char(marker(j)),'Color','b', 'MarkerFaceColor', 'b')
+        plot(mean_STATICzshim(i:i+11),char(marker(j)),'Color','r', 'MarkerFaceColor', 'r')
+        plot(mean_rtshim(i:i+11),char(marker(j)),'Color','g', 'MarkerFaceColor', 'g')
+        title(strcat('TE',num2str(j)));
+        xlabel('slice number');
+        ylabel('signal [arb]');
+        hold off;
+        j=(j+1);
+    end
+    legend('no shim','static z-shim', 'rt shim');
+    saveas(figure1, ['fig1_',char(dir_list(k).name),'.fig'])
+   
+    
+    figure2 = figure; hold on;    
+    for i = 1:12
+        subplot(6,2,i); 
+        hold on;
+        plot(NOshim.WA__(i:12:end),'-o','Color','b', 'MarkerFaceColor', 'b')
+        plot(mean_STATICzshim(i:12:end),'-x','Color','r', 'MarkerFaceColor', 'r')
+        plot(mean_rtshim(i:12:end),'-s','Color','g', 'MarkerFaceColor', 'g')
+        title(strcat('slice',num2str(i)));
+        xlabel('echo number');
+        ylabel('signal [arb]');
+        hold off;
+    end
+    legend('no shim','static z-shim', 'rt shim');
+    %sgtitle('Signal decay vs. echo number for slices 1 - 12'); 
+    saveas(figure2, ['fig2_',char(dir_list(k).name),'.fig'])
+
+end
